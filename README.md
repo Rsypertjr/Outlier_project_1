d329d4b1b2048ca5bce54babdd94db7019f9179f
Richard L. Sypert Jr <rsypertjr@hotmail.com>
2024-10-08 08:50:26 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Outlier_project_1
---
+++ app-20241008T143330Z-001/app/build/precache-manifest.a05ea467647ae37d0f9fc34f9d4f2c28.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "705d678cb17a2d47b8ea",
+    "url": "/static/js/main.705d678c.chunk.js"
+  },
+  {
+    "revision": "d0a3f341f871d32b7d93",
+    "url": "/static/css/1.09205a6b.chunk.css"
+  },
+  {
+    "revision": "d0a3f341f871d32b7d93",
+    "url": "/static/js/1.d0a3f341.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "aef464c594c34fa71eb894afc02be301",
+    "url": "/index.html"
+  }
+];
